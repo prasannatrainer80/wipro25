4b416fab1f3c837398af6cf11be5f02af06e1eb2
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-02-10 16:43:27 +0530

    code

---
+++ Feb10_par2/LibraryProject/LibraryProject/BookDao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject
+{
+    internal class BookDao
+    {
+        static List<Book> booksList;
+
+        static BookDao() {
+            booksList = new List<Book>();
+        }
+
+        public string AddBook(Book book)
+        {
+            booksList.Add(book);
+            return "book added successfully...";
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return booksList;
+        }
+
+    }
+}
---
+++ Feb10_par2/LibraryProject/LibraryProject/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace LibraryProject
+{
+    internal class Program
+    {
+        static BookDao bookDao;
+
+        static Program()
+        {
+            bookDao = new BookDao();
+        }
+        public static void AddBookMain()
+        {
+            Book book = new Book();
+            Console.WriteLine("Enter Book Id  ");
+            book.BookId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Title  ");
+            book.Title = Console.ReadLine();
+            Console.WriteLine("Enter Author  ");
+            book.Author = Console.ReadLine();
+            Console.WriteLine("Enter No.of Copies  ");
+            book.NoOfCopies = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(bookDao.AddBook(book));
+        }
+
+        public static void ShowBookMain()
+        {
+            List<Book> books = bookDao.GetAllBooks();
+            foreach (Book book in books)
+            {
+                Console.WriteLine("Book Id " +book.BookId + 
+                    " Book Title " +book.Title + 
+                    " Book Author " +book.Author + 
+                    "No.Of Copies " +book.NoOfCopies);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Add Book");
+                Console.WriteLine("2. Show Book");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("Enter Choice  ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1: AddBookMain(); break;
+                    case 2: ShowBookMain(); break;
+                    case 6: return;
+                }
+            } while (choice != 6);
+        }
+    }
+}
